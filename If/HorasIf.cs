c28ed2efa1d70dd446933687615dfe6ec88e1627
Henrique Nunes Guilhem <henriqueguilhem77@gmail.com>
2023-08-03 20:55:34 -0300

    Organizaçao

---
+++ If/HorasIf.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Que horas sao?");
+int horas = int.Parse(Console.ReadLine());
+
+if (horas < 12)
+{
+    Console.WriteLine("Bom dia!")
+}
+else if (horas < 18){
+    Console.WriteLine("Boa tarde!")
+}
+else
+{
+    Console.WriteLine("Boa noite!")
+}
