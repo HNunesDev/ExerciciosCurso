8be2583951a8a384a48e06b7893e36220ed7d793
Henrique Nunes Guilhem <henriqueguilhem77@gmail.com>
2023-08-03 15:19:01 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosCurso/DentroForaFor.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Quantos números inteiros serao lidos? ");
+int x = int.Parse(Console.ReadLine());
+
+int dentro = 0;
+int fora = 0;
+for (int i = 1; i <= x; i++)
+{
+    Console.WriteLine("Digite um número: ");
+    int numero = int.Parse(Console.ReadLine());
+    if (numero >= 10 && numero <= 20)
+    {
+        dentro = dentro + 1;
+    }
+    else
+    {
+        fora = fora + 1;
+    }
+}
+Console.WriteLine("Números dentro do intervalo: " + dentro);
+Console.WriteLine("Números fora do intervalo: " + fora);
---
+++ While/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Entre com o código do combustivel");
+int combustivel = int.Parse(Console.ReadLine());
+int alcool = 0;
+int gasolina = 0;
+int dissel = 0;
+
+while (combustivel != 4)
+{
+    if (combustivel == 1)
+    {
+        alcool = alcool + 1;
+    }
+    else if (combustivel == 2)
+    {
+        gasolina = gasolina + 1;
+    }
+    else if (combustivel == 3)
+    {
+        dissel = dissel + 1;
+    }
+
+    combustivel = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Alcool: " + alcool);
+Console.WriteLine("Gasolina: " + gasolina);
+Console.WriteLine("Dissel: " + dissel);
