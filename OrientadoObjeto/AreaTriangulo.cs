e57997874f800d3615bc0d647ddfc86eb671c294
Henrique Nunes Guilhem <henriqueguilhem77@gmail.com>
2023-08-07 16:26:57 -0300

    Melhoria no código do triangulo, atribuindo um método para calcular a área do triangulo

--- OrientadoObjeto/AreaTriangulo.cs
+++ OrientadoObjeto/AreaTriangulo.cs
@@ -2,21 +2,19 @@
 
 Triangulo X = new Triangulo();
 Triangulo Y = new Triangulo();
+
 Console.WriteLine("Entre com as medidas do triângulo X:");
 X.A = double.Parse(Console.ReadLine());
 X.B = double.Parse(Console.ReadLine());
 X.C = double.Parse(Console.ReadLine());
+
 Console.WriteLine("Entre com as medidas do triângulo Y:");
 Y.A = double.Parse(Console.ReadLine());
 Y.B = double.Parse(Console.ReadLine());
 Y.C = double.Parse(Console.ReadLine());
-double p = (X.A + X.B + X.C) / 2.0;
 
-double areaX = Math.Sqrt(p * (p - X.A) * (p - X.B) * (p - X.C));
-
-p = (Y.A + Y.B + Y.C) / 2.0;
-
-double areaY = Math.Sqrt(p * (p - Y.A) * (p - Y.B) * (p - Y.C));
+double areaX = X.Area();
+double areaY = Y.Area();
 
 Console.WriteLine("Área de X = " + areaX.ToString("F4"));
 Console.WriteLine("Área de Y = " + areaY.ToString("F4"));
--- OrientadoObjeto/Triangulo.cs
+++ OrientadoObjeto/Triangulo.cs
@@ -5,5 +5,12 @@
         public double A;
         public double B;
         public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+
+        }
     }
 }
