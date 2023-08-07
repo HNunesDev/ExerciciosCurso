7db36bc4e9499f44e6526cbeee695fa25cb35349
Henrique Nunes Guilhem <henriqueguilhem77@gmail.com>
2023-08-07 18:18:21 -0300

    Atualizaçao do codigo na utilizaçao do .ToString

---
+++ Estoque/Estoque.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Estoque;
+Sistema sistema = new Sistema();
+
+Console.WriteLine("Entre com os dados do produto: ");
+Console.Write("Nome: ");
+sistema.Nome = Console.ReadLine();
+Console.Write("Preço: ");
+sistema.Preco = double.Parse(Console.ReadLine());
+Console.Write("Quantidade: ");
+sistema.Quantidade = int.Parse(Console.ReadLine());
+double estoque = sistema.ValorEmEstoque();
+
+Console.WriteLine("Dados do produto: " + sistema);
+
---
+++ Estoque/Sistema.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Estoque
+{
+    internal class Sistema
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome + ", $" + Preco + ", " + Quantidade  + " unidades, Valor total $ " + ValorEmEstoque().ToString("F2");
+        }
+    }
+}
