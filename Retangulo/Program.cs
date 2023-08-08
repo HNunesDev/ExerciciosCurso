d5d6ad0d2e5f6020a8a1a637d68bb2f7f16e151f
Henrique Nunes Guilhem <henriqueguilhem77@gmail.com>
2023-08-08 17:41:53 -0300

    Calculando a Area, Perimetro e diagnonal de um retangulo com orientaçao a objeto

---
+++ Retangulo/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Retangulo;
+
+Retanguloo X = new Retanguloo();
+
+
+Console.WriteLine("Entre com a altura e largura do Retangulo: ");
+Console.Write("Largura: ");
+X.Largura = double.Parse(Console.ReadLine());
+Console.Write("Altura: ");
+X.Altura = double.Parse(Console.ReadLine());
+
+double areaX = X.Area();
+double perimetroX = X.Perimetro();
+double diagonalX = X.Diagonal();
+
+Console.WriteLine(X);
---
+++ Retangulo/Retangulo.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace Retangulo
+{
+    public class Retanguloo
+    {
+        public double Largura;
+        public double Altura;
+
+
+        public double Area()
+        {
+            double a = Largura * Altura;
+            return a;
+        }
+        public double Perimetro()
+        {
+            double p = (Largura *2) + (Altura * 2);
+            return p;
+        }
+        public double Diagonal()
+        {
+            double d = Math.Sqrt(Math.Pow(Largura, 2) + Math.Pow(Altura, 2));
+            return d;
+        }
+
+        public override string ToString()
+        {
+            return "A Area do retangulo é: " + Area().ToString("F2") + " Seu perimetro: " + Perimetro().ToString("F2") + " E por fim sua diagnonal: " + Diagonal().ToString("F2");
+        }
+    }
+}
