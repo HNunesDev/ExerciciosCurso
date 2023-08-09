2ede2329af837499deab94f5c357534f8256dbb2
Henrique Nunes Guilhem <henriqueguilhem77@gmail.com>
2023-08-09 20:28:22 -0300

    Programa para calcular a nota final com base em três notas dadas pelo usuário, e retornar se o aluno foi aprovado e em caso de reporvaçao, quanto faltou para ser aprovado.

---
+++ Aluno/Notas.cs
@@ -0,0 +1,22 @@
+﻿namespace Aluno
+{
+    internal class Notas
+    {
+        public string Nome;
+        public double NotaUm;
+        public double NotaDois;
+        public double NotaTres;
+
+        public double Media()
+        {
+            double M = NotaUm + NotaDois + NotaTres;
+            return M;
+        }
+
+        public override string ToString()
+        {
+            return "Aluno: " + Nome + "  Nota Final: " + Media().ToString("F2");
+        }
+
+    }
+}
---
+++ Aluno/Program.cs
@@ -0,0 +1,29 @@
+﻿using Aluno;
+using System.Net.Http.Headers;
+
+Notas AlunoUm = new Notas();
+
+Console.WriteLine("Entre com o nome do aluno e suas três notas: ");
+Console.Write("Nome: ");
+AlunoUm.Nome = Console.ReadLine();
+Console.Write("Primeira nota: ");
+AlunoUm.NotaUm = double.Parse(Console.ReadLine());
+Console.Write("Segunda Nota: ");
+AlunoUm.NotaDois = double.Parse(Console.ReadLine());
+Console.Write("Terceira Nota: ");
+AlunoUm.NotaTres = double.Parse(Console.ReadLine());
+
+
+Console.WriteLine(AlunoUm);
+
+double midia = AlunoUm.Media();
+if (midia < 60.00)
+{
+    Console.WriteLine("Reporvado");
+    double faltante = midia - 60.00;
+    Console.WriteLine("Faltaram: " +  faltante + "pontos");
+}
+else
+{
+    Console.WriteLine("Aprovado");
+}
