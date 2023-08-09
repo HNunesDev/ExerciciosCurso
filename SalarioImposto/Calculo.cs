bde0a9071e27db9f66bcb1c2ecb285cdbacd048a
Henrique Nunes Guilhem <henriqueguilhem77@gmail.com>
2023-08-08 23:22:09 -0300

    Exercicio para ler o salario do funcionario e aplicar uma porcentagem de aumento através de atributos e métodos.

---
+++ SalarioImposto/Calculo.cs
@@ -0,0 +1,26 @@
+﻿namespace SalarioImposto
+{
+    internal class Calculo
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+
+        public double SalarioLiquido()
+        {
+            double valor = SalarioBruto - Imposto;
+            return valor;
+        } 
+        public void AumentarSalario(double porcentagem)
+        {
+             SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem) / 100;           
+        }
+
+
+        public override string ToString()
+        {
+            return "Funcionário: " + Nome + ", $" + SalarioLiquido().ToString("F2");
+        }
+    }
+}
---
+++ SalarioImposto/Program.cs
@@ -0,0 +1,19 @@
+﻿using SalarioImposto;
+
+Calculo conta = new Calculo();
+Console.WriteLine("Entre com os dados do funcionário: ");
+Console.Write("Nome: ");
+conta.Nome = Console.ReadLine();
+Console.Write("Salário: ");
+conta.SalarioBruto = double.Parse(Console.ReadLine());
+Console.Write("Imposto: ");
+conta.Imposto = double.Parse(Console.ReadLine());
+    
+Console.WriteLine(" ");
+Console.WriteLine(conta);
+
+Console.Write("Digite a porcentagem para aumentar o salário: ");
+double Numero = double.Parse(Console.ReadLine());
+conta.AumentarSalario(Numero);
+
+Console.WriteLine("Dados atualizados: " + conta);
