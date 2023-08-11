874083d1eb0f7724c47a62dc1da3e8d75cb0c5fb
Henrique Nunes Guilhem <henriqueguilhem77@gmail.com>
2023-08-11 19:20:28 -0300

    Utilização de membros estáticos para gerar a compra de dolar de acordo com o cotação inserida pelo usuário e mostrar o valor a ser pago em reais

---
+++ ConversorDeMoeda/Cambio.cs
@@ -0,0 +1,16 @@
+﻿namespace ConversorDeMoeda
+{
+    internal class Cambio
+    {
+        public static double Dolar;
+        public static double Quantidade;
+        public static double Troca(double dolar, double quantidade)
+        {
+            double valor = dolar * quantidade;
+            double valorFinal = (valor * 6 / 100) + valor;
+            return valorFinal;
+        }
+
+        
+    }
+}
---
+++ ConversorDeMoeda/Program.cs
@@ -0,0 +1,9 @@
+﻿using ConversorDeMoeda;
+
+Console.Write("Qual a cotação do dolar: ");
+Cambio.Dolar = double.Parse(Console.ReadLine());
+Console.Write("Quantos doláres ira comprar: ");
+Cambio.Quantidade = double.Parse(Console.ReadLine());
+
+
+Console.WriteLine("O valor a ser pago é de: $" + Cambio.Troca(Cambio.Dolar, Cambio.Quantidade).ToString("F2"));
