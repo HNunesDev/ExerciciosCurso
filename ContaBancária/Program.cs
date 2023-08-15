a9be74893766929ee4d52cf1835c5fea9fd741c8
Henrique Nunes Guilhem <henriqueguilhem77@gmail.com>
2023-08-15 18:22:36 -0300

    Desenvolvimento de sistema de conta bancário que le conta, adiciona e retira saldo. Utilizando o conceito de encapsulamento.

---
+++ ContaBancária/Program.cs
@@ -0,0 +1,36 @@
+﻿using ContaBancária;
+
+Sistema sistemaBancario;
+
+Console.WriteLine("Entre com as informações da conta: ");
+Console.Write("Número da conta: ");
+int numero = int.Parse(Console.ReadLine());
+Console.Write("Titular da conta: ");
+string nome = Console.ReadLine();
+Console.Write("Haverá depósito inicial? (s/n) ");
+char resposta = char.Parse(Console.ReadLine());
+if (resposta == 's')
+{
+    Console.Write("Valor do depósito inicial: ");
+    double depositoInicial = double.Parse(Console.ReadLine());
+    sistemaBancario = new Sistema(numero, nome, depositoInicial);
+}
+else
+{
+    sistemaBancario = new Sistema(numero, nome);
+}
+
+Console.WriteLine(sistemaBancario);
+
+Console.WriteLine("Entre com um valor para deposito: ");
+double deposito =  double.Parse(Console.ReadLine());
+sistemaBancario.AdicionarSaldo(deposito);
+
+Console.WriteLine(sistemaBancario);
+
+Console.WriteLine("Entre com um valor para saque: ");
+double saque = double.Parse(Console.ReadLine());
+sistemaBancario.SaqueSaldo(saque);
+
+Console.WriteLine(sistemaBancario);
+
---
+++ ContaBancária/Sistema.cs
@@ -0,0 +1,33 @@
+﻿namespace ContaBancária
+{
+    internal class Sistema
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public Sistema(int conta, string nome)
+        {
+            Numero = conta;
+            Titular = nome;
+        }
+        public override string ToString()
+        {
+            return "Conta: " + Numero + ", titular: " + Titular + ", Saldo: R$ " + Saldo.ToString("F2");
+        }
+        public Sistema(int conta, string nome, double deposito) : this(conta, nome) {
+            AdicionarSaldo(deposito);
+        }
+
+        public void AdicionarSaldo(double saldoAdicional)
+        {
+            Saldo = Saldo + saldoAdicional;
+   
+        }
+        public void SaqueSaldo(double saque)
+        {
+            Saldo = (Saldo - saque) - 5;
+        }
+
+    }
+} 
