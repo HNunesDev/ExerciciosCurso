a2c900baae488097a628fe7578020567abd4e12d
Henrique Nunes Guilhem <henriqueguilhem77@gmail.com>
2023-08-21 16:23:26 -0300

    Exercicio utilizando vetor e for

---
+++ Vetor/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Quantas alturas iremos ler? ");
+int n = int.Parse(Console.ReadLine());
+
+double[] vect = new double[n];
+
+for (int i = 0; i < n; i++)
+{
+    vect[i] = double.Parse(Console.ReadLine());
+}
+
+double soma = 0.0;
+for (int i = 0;i < n; i++)
+{
+    soma += vect[i];
+}
+double media = soma / n;
+Console.WriteLine("A média das alturas informadas é: " +  media.ToString("F2"));
